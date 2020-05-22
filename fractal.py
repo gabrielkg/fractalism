0389fe752a9238a8ad2891439eeffbb630050531
Gabriel Keeble-Gagnere <gabriel7@gmail.com>
2020-05-22 15:38:12 +1000

    Fix algorithm bug: need to add resulting vector to current position!

--- fractal.py
+++ fractal.py
@@ -5,10 +5,10 @@ import matplotlib as mpl
 
 mpl.use('Cairo')
 
-width=100
+width=200
 corner=width/2
 
-fig = plt.figure(figsize=(25, 25))
+fig = plt.figure(figsize=(20, 20))
 ax = fig.add_axes([0, 0, 1, 1], frameon=False)
 ax.set_xlim(-corner, corner), ax.set_xticks([])
 ax.set_ylim(-corner, corner), ax.set_yticks([])
@@ -24,19 +24,19 @@ with open(sys.argv[1], "r") as fh:
     seq = fh.readline().strip()
     size = len(seq)
     count = 0
-    for bp in seq:
+    for bp in seq[:10000000]:
         if bp == "A":
-            x_now = (corner-x_now)/2
-            y_now = (corner-y_now)/2
+            x_now += (corner-x_now)/2
+            y_now += (corner-y_now)/2
         elif bp == "T":
-            x_now = (-corner-x_now)/2
-            y_now = (corner-y_now)/2
+            x_now += (-corner-x_now)/2
+            y_now += (corner-y_now)/2
         elif bp == "C":
-            x_now = (corner-x_now)/2
-            y_now = (-corner-y_now)/2
+            x_now += (corner-x_now)/2
+            y_now += (-corner-y_now)/2
         elif bp == "G":
-            x_now = (-corner-x_now)/2
-            y_now = (-corner-y_now)/2
+            x_now += (-corner-x_now)/2
+            y_now += (-corner-y_now)/2
         x.append(x_now)
         y.append(y_now)
         count += 1
@@ -47,7 +47,7 @@ with open(sys.argv[1], "r") as fh:
 ax.plot([round(a,2) for a in x],
         [round(b,2) for b in y],
         'b,', # blue pixels
-        alpha=.5,
-        antialiased=False)
+        alpha=0.2,
+        antialiased=True)
 
 plt.savefig('cairo.png')

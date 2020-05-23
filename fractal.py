cef2baf112abc930e56620935299b9ed72c0b24e
Gabriel Keeble-Gagnere <gabriel7@gmail.com>
2020-05-23 14:04:48 +1000

    Use argparse to parse arguments.

--- fractal.py
+++ fractal.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools, sys
 import matplotlib as mpl
+import argparse
 
 def close_window(evt):
     exit()
@@ -30,12 +31,30 @@ y=[]
 # for experiments
 factor=2
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", type=str,
+                    help="input file")
+parser.add_argument("-s", "--start", type=int,
+                    help="start position (bp)",
+                    default=1)
+parser.add_argument("-e", "--end", type=int,
+                    help="end position (bp)",
+                    default=1000000)
+parser.add_argument("-a", "--alpha", type=float,
+                    help="alpha value for points",
+                    default=0.05)
+parser.add_argument("--step", type=int,
+                    help="sample every <step> (bp)",
+                    default=1)
+args = parser.parse_args()
+
 # expects a file containing a string of ACGT...
-with open(sys.argv[1], "r") as fh:
+with open(args.file, "r") as fh:
     seq = fh.readline().strip()
     size = len(seq)
+    start, end = args.start, min(args.end, size)
     count = 0
-    for bp in seq[:5000000]:
+    for bp in seq[args.start-1:end:args.step]:
         if bp in ["A", "C", "G", "T"]:
             if bp == "T": # top right
                 x_now += (corner-x_now)/factor
@@ -58,10 +77,11 @@ with open(sys.argv[1], "r") as fh:
                 ax.plot(x,
                         y,
                         '.', # points
-                        alpha=0.1,
+                        alpha=args.alpha,
                         markersize=2,
                         antialiased=True,
-                        color='#003366')
+                        color='#035efc')
+                        #color='#003366')
                 plt.show()
                 plt.pause(0.0001)
                 x = []

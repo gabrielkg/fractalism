c78f69fe71a914fb8d8182c69835936d52c3b6a4
Gabriel Keeble-Gagnere <gabriel7@gmail.com>
2020-05-24 16:35:14 +1000

    Let user set figure width and height.

--- fractal.py
+++ fractal.py
@@ -8,29 +8,9 @@ from matplotlib.animation import FFMpegWriter
 
 mpl.use("Qt5Agg")
 
+# exit on window close
 def close_window(evt):
     exit()
-
-width=200
-corner=width/2
-
-fig = plt.figure(figsize=(10, 10))
-
-# connect close window event to handler function
-fig.canvas.mpl_connect('close_event', close_window)
-
-ax = fig.add_axes([0, 0, 1, 1], frameon=False)
-ax.set_xlim(-corner, corner), ax.set_xticks([])
-ax.set_ylim(-corner, corner), ax.set_yticks([])
-
-x_now = 0
-y_now = 0
-
-x=[]
-y=[]
-
-# for experiments
-factor=2
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", type=str,
@@ -53,9 +33,38 @@ parser.add_argument("--animate",
                     default=False)
 parser.add_argument("--animateout", type=str,
                     help="output animation to file")
+parser.add_argument("--figwidth", type=int,
+                    help="figure width",
+                    default=10)
+parser.add_argument("--figheight", type=int,
+                    help="figure height",
+                    default=10)
 args = parser.parse_args()
 
-print(mpl.get_backend())
+print("Using backend: ", mpl.get_backend())
+
+width=200
+corner=width/2
+
+# create figure
+fig = plt.figure(figsize=(args.figwidth,
+                          args.figheight))
+
+# connect close window event to handler function
+fig.canvas.mpl_connect('close_event', close_window)
+
+ax = fig.add_axes([0, 0, 1, 1], frameon=False)
+ax.set_xlim(-corner, corner), ax.set_xticks([])
+ax.set_ylim(-corner, corner), ax.set_yticks([])
+
+x_now = 0
+y_now = 0
+
+x=[]
+y=[]
+
+# for experiments
+factor=2
 
 def calculateNext(bp, x_now, y_now, corner, factor):
     x_next = x_now
@@ -72,7 +81,7 @@ def calculateNext(bp, x_now, y_now, corner, factor):
     elif bp == "G": # bottom left
         x_next += (-corner-x_now)/factor
         y_next += (-corner-y_now)/factor
-    return x_next, y_next
+    return round(x_next,1), round(y_next,1)
 
 # expects a file containing a string of ACGT...
 with open(args.file, "r") as fh:
@@ -83,9 +92,9 @@ with open(args.file, "r") as fh:
         graph, = ax.plot([],
                          [],
                          '.',
-                         markersize=2,
+                         markersize=1,
                          color='#035efc',
-                         alpha=0.1)
+                         alpha=args.alpha)
         def animation(i):
             global x
             global y
@@ -93,8 +102,8 @@ with open(args.file, "r") as fh:
             global y_now
             x_buff = []
             y_buff = []
-            step = 2000
-            for bp in seq[(i-1)*step:(i*step)-1]:
+            step = 10000
+            for bp in seq[start+(i-1)*step:start+(i*step)-1]:
                 if bp in ["A", "C", "G", "T"]:
                     x_now, y_now = calculateNext(bp,
                                                  x_now,
@@ -106,11 +115,22 @@ with open(args.file, "r") as fh:
             x += x_buff
             y += y_buff
             graph.set_data(x, y)
+            sys.stdout.write(".")
+            sys.stdout.flush()
             return graph,
-        ani = FuncAnimation(fig, animation, frames=500, interval=3)
+        ani = FuncAnimation(fig,
+                            animation,
+                            frames=50,
+                            interval=1,
+                            blit=True)
         if args.animateout:
-            FFwriter = FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
-            ani.save('fractal.mp4', writer=FFwriter)
+            #FFwriter = FFMpegWriter(fps=30,
+            #                         extra_args=['-vcodec',
+            #                                     'libx264'])
+            #ani.save('fractal.mp4', writer=FFwriter)
+            ani.save('fractal.gif',
+                     dpi=80,
+                     writer='imagemagick')
         else:
             plt.show()
     else:
